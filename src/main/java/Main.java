2f785526771a3f2d9787a72efe43784a08c410e3
ballatamas98 <36437827+ballatamas98@users.noreply.github.com>
2022-10-01 16:59:34 +0200

    refactor

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ import formatter.ReadableDurationFormatter;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(ReadableDurationFormatter.format(31_556_061));
+        System.out.println(ReadableDurationFormatter.format(Integer.parseInt(args[0])));
     }
 
 
